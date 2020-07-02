f4f088f0fe9ae4baeacb1c874b8181d8446540d0
Anant Thazhemadam <anant.thazhemadam@gmail.com>
2020-07-03 02:24:11 +0530

    Render Names of All characters
    Using Axios, a request is made to the Breaking Bad API. The names
    received is then displayed onto the screen.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 import './App.css';
+import Header from './components/ui/Header';
+import CharacterGrid from './components/characters/CharacterGrid'
+
 
 const App = () => {
+  const [items, setItems]  = useState([]);
+  const [isLoading, setIsLoading]  = useState(true);
+  
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`);
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+
+  }, []);
+
   return (
     <div className="container">
-      Hello!
+      <Header />
+      <CharacterGrid isLoading={isLoading}  items={items}/>
     </div>
   );
 }
